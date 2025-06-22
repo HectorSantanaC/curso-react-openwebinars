12b985388fba9fa11b8d355c242f27d24dab3daf
HectorSantanaC <hector.santana.gc@gmail.com>
2025-06-22 11:19:07 +0100

    Componentes react

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import HeaderComponent from './components/HeaderComponent'
+import ButtonComponent from './components/ButtonComponent'
+
+function App() {
+  //let number = 0;
+
+  const [number, setNumber] = useState(0);
+  const [myValue, setMyValue] = useState("");
+  let myPlaceholder = "Escribe aquí";
+
+  const addOne = () => {
+    //number++;
+    setNumber(number + 1);
+    console.log(number);
+  }
+
+  const sayHello = () => {
+    console.log("Hello!");
+  }
+
+const handleChange = (e) => {
+  console.log(e.target.value);
+}
+
+  return (
+    <>
+      <HeaderComponent></HeaderComponent>
+
+      <main className='main-content'>
+        <h2 onClick={sayHello}>Hola a todos!</h2>
+
+        <h2 onClick={addOne}>Number: {number}</h2>
+
+        <input value={myValue} placeholder={myPlaceholder} type="text" onChange={handleChange}/>
+
+        <br />
+        <br />
+        <ButtonComponent></ButtonComponent>
+      </main>
+    </>
+  )
+}
+
+export default App
