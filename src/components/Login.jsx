958b8251689a3bd206c3c7ec4426b7fe59257d4c
HectorSantanaC <hector.santana.gc@gmail.com>
2025-06-24 13:24:44 +0100

    Formularios en React

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,19 +1,41 @@
 function Login(props) {
   const user = {
-    username: "Héctor",
-    email: "hector@email.es"
+    username: "",
+    email: "",
   };
 
-  const handleClick = () => {
+  const setUsername = (e) => {
+    user.username = e.target.value;
+  };
+
+    const setEmail = (e) => {
+    user.useremail = e.target.value;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); // Evita que se el efecto por defecto del formulario (recargar la página)
+    console.log(user);
     props.handleLogin(user);
-  }
+  };
 
   return (
     <section>
       <h2>Login section</h2>
-      <button onClick={handleClick}>Login</button>
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <label htmlFor="username">Username</label>
+          <input type="text" id="username" onChange={setUsername} />
+        </fieldset>
+
+        <fieldset>
+          <label htmlFor="email">Email</label>
+          <input type="text" id="email" onChange={setEmail} />
+        </fieldset>
+        
+        <button>Login</button>
+      </form>
     </section>
-  )
+  );
 }
 
-export default Login
+export default Login;
